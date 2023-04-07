a5195268d40f21bcd80f93cde11c926c5acffa6d
siwale kamasatjakul <siwale.kama@gmail.com>
2023-04-07 10:14:54 +0700

    init CI workflow

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 type Store struct {
@@ -18,8 +19,8 @@ func NewStore(db *sql.DB) *Store { //use sql.DB as input
 	}
 }
 
-func (store *Store) execTx(ctx context.Context,fn func(*Queries) error) error{ // takes a context and a callback function as input, then it will start a new db transaction
-	tx,err := store.db.BeginTx((ctx, &sql.TxOptions{})) //start a new transaction we call store.db.BeginTx() pass in the context, and optionally a sql.TxOptions
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error { // takes a context and a callback function as input, then it will start a new db transaction
+	tx, err := store.db.BeginTx(ctx, &sql.TxOptions{}) //start a new transaction we call store.db.BeginTx() pass in the context, and optionally a sql.TxOptions
 	// this option allow us to set a custom isolation level for this transaction
 	if err != nil {
 		return err
@@ -28,42 +29,41 @@ func (store *Store) execTx(ctx context.Context,fn func(*Queries) error) error{ /
 	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
-			return fmt.Errorf("tx err: %v, rb err: %v",err,rbErr)
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
 		return err
 	}
 	return tx.Commit()
 }
 
-type TransferTxParams struct{
-	FromAccountID int64 'json:"from_account_id'
-	ToAccountID int64 'json:"to_account_id"'
-	Amount int64 'json:"amount"'
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
 }
 
 type TransferTxResult struct {
-    Transfer    Transfer `json:"transfer"`
-    FromAccount Account  `json:"from_account"`
-    ToAccount   Account  `json:"to_account"`
-    FromEntry   Entry    `json:"from_entry"` // records that money is moving out of the FromAcc 
-    ToEntry     Entry    `json:"to_entry"` //records that money is moving in of the FromAcc 
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"` // records that money is moving out of the FromAcc
+	ToEntry     Entry    `json:"to_entry"`   //records that money is moving in of the FromAcc
 }
 
-func (store *Store) TransfereTx(ctx context.Context, arg TransferTxParams){
+func (store *Store) TransfereTx(ctx context.Context, arg TransferTxParams) {
 	var result TransferTxResult
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
-            FromAccountID: arg.FromAccountID,
-            ToAccountID:   arg.ToAccountID,
-            Amount:        arg.Amount,
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
 
+		}
+		return nil
 	})
-	if err != nil{
-	return err 
-
-	}
-	return nil
-})
-return result,err
+	return result, err
 }
